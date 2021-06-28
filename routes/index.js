b15cc58cc530491e82acc2bee1476a4744ee0936
John Ho <xiangkee.ho@hotmail.com>
2021-06-28 12:07:59 +0800

    20210628

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const config = require('../config.json');
 const jwt = require('jsonwebtoken');
-const connection = require('../db.config');
 const routes = require('express').Router();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'gm1vm9miljet6rmv',
+  password: 'kvg660giecd1gmvq',
+  database: 'a7s7bdoaogdxt7kp',
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+});
 
 routes.post('/authenticate', (req, res) => {
   let users = req.body;
